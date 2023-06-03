978a931f37540ef4b7f3ead830a1f5f78aa7f91d
Eliška Korencová <eli.korenc@gmail.com>
2023-06-03 17:45:42 +0200

    Přidání funkce onClick na tlačítka v komponentě Joke.

--- src/components/Joke/index.jsx
+++ src/components/Joke/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 import './style.css';
 
 export const Joke = ({ avatar, name, text, likes, dislikes }) => {
+  const [likesNumber, setLikesNumber] = useState(likes);
+  const [dislikesNumber, setDislikesNumber] = useState(dislikes);
+
   return (
     <div key={name} className="container">
       <div className="joke">
@@ -14,13 +18,21 @@ export const Joke = ({ avatar, name, text, likes, dislikes }) => {
           <p className="joke__text">{text}</p>
         </div>
         <div className="joke__likes">
-          <button id="btn-up" className="btn-like btn-like--up"></button>
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={() => setLikesNumber(likesNumber + 1)}
+          ></button>
           <span id="likes-up" className="likes-count likes-count--up">
-            {likes}
+            {likesNumber}
           </span>
-          <button id="btn-down" className="btn-like btn-like--down"></button>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={() => setDislikesNumber(dislikesNumber + 1)}
+          ></button>
           <span id="likes-down" className="likes-count likes-count--down">
-            {dislikes}
+            {dislikesNumber}
           </span>
         </div>
       </div>
